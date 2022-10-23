1ffec869ece894ef938c7a08e0805b27a3528544
Aidar06 <kabylbekovajdar94@gmail.com>
2022-10-23 11:38:34 +0600

    second commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.7,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
